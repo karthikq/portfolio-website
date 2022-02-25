0ad0b1270e0ad872911336f27f254794b1f6ec6c
karthikq <kk968346@gmail.com>
2022-02-25 17:19:21 +0530

    projects4

--- src/components/projects/Porject.jsx
+++ src/components/projects/Porject.jsx
@@ -1,21 +1,41 @@
 /** @format */
-import React from "react";
+import React, { useState } from "react";
 import "./projects.styles.scss";
 
 const Porject = () => {
+  const [item, setItem] = useState("React");
+
   return (
     <div className="projects-container">
       <h1 className="header-tag">Projects</h1>
       <div className="projects-contents">
         <div className="projects">
           <header className="projects-contents-header">
-            <div className="project-item-header">
+            <div
+              className={
+                item === "React"
+                  ? "project-item-headeractive"
+                  : "project-item-header"
+              }
+              onClick={() => setItem("React")}>
               <span>React</span>
             </div>
-            <div className="project-item-header">
+            <div
+              className={
+                item === "Node"
+                  ? "project-item-headeractive"
+                  : "project-item-header"
+              }
+              onClick={() => setItem("Node")}>
               <span>Node</span>
             </div>
-            <div className="project-item-header">
+            <div
+              className={
+                item === "Mern"
+                  ? "project-item-headeractive"
+                  : "project-item-header"
+              }
+              onClick={() => setItem("Mern")}>
               <span>Mern</span>
             </div>
           </header>
