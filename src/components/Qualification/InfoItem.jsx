b69c0870b41a2708d9fe1ccfaca0e91564ae8b3d
karthikq <kk968346@gmail.com>
2022-03-01 17:17:40 +0530

    prject page5

--- src/components/Qualification/InfoItem.jsx
+++ src/components/Qualification/InfoItem.jsx
@@ -24,7 +24,7 @@ const InfoItem = ({ data, inView, state }) => {
         viewBox="0 0 19 19"
         fill="none"
         xmlns="http://www.w3.org/2000/svg">
-        <circle cx="9.5" cy="9.5" r="9.5" fill="#E754BD" />
+        <circle cx="9.5" cy="9.5" r="9.5" fill="#119dfa" />
       </svg>
       <h6>{data.header}</h6>
       <span>{data.inst}</span>
