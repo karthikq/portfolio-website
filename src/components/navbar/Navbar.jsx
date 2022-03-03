0f63c2293c341ad4acb1e0dbbcc0e1bb24d3921d
karthikq <kk968346@gmail.com>
2022-03-03 12:53:31 +0530

    vanta4

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,63 @@
 /** @format */
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Navbar.styles.scss";
 import Navbarli from "./Navbarli";
 
 const Navbar = () => {
   const nav = useRef();
+  const [pathValue, setPathValue] = useState();
 
   document.addEventListener("scroll", (e) => {
-    const contains = nav.current.classList.contains("navbar-active");
-
     if (window.scrollY > 400) {
       nav.current.classList.add("navbar-active");
     }
-    if (window.scrollY <= 400) {
+    if (window.scrollY < 400) {
       nav.current.classList.remove("navbar-active");
     }
   });
+
+  document.addEventListener("click", (e) => {
+    // if (e.target.contains(nav)) {
+    //   console.log("S");
+    // } else {
+    //   console.log("s");
+    // }
+    console.log(e.target.contains("navbar-contents"));
+  });
+
   return (
     <div ref={nav} className="navbar-container">
       <div className="navbar-contents">
         <ul className="ul-nav">
-          <Navbarli pathname={"About"} />
+          <a
+            className={pathValue === "about" ? "li-navactive" : ""}
+            href="#about"
+            onClick={() => setPathValue("about")}>
+            <li>About </li>
+          </a>
+          <a
+            className={pathValue === "services" ? "li-navactive" : ""}
+            href="#services"
+            onClick={() => setPathValue("services")}>
+            <li>Services </li>
+          </a>
+          <a
+            className={pathValue === "projects" ? "li-navactive" : ""}
+            href="#projects"
+            onClick={() => setPathValue("projects")}>
+            <li>Projects </li>
+          </a>
+          <a
+            className={pathValue === "contact" ? "li-navactive" : ""}
+            href="#contact"
+            onClick={() => setPathValue("contact")}>
+            <li>Contact</li>
+          </a>
+          {/* <Navbarli pathname={"About"} />
           <Navbarli pathname={"Portfolio"} />
           <Navbarli pathname={"Services"} />
-          <Navbarli pathname={"Contact"} />
+          <Navbarli pathname={"Contact"} /> */}
         </ul>
       </div>
     </div>
--- src/components/navbar/Navbarli.jsx
+++ src/components/navbar/Navbarli.jsx
@@ -6,20 +6,26 @@ const Navbarli = ({ pathname, location }) => {
   const [pathValue, setPathValue] = useState("");
   const ref = useRef();
 
-  //   const handlePath = () => {
-  //     window.history.pushState("s", "", "?path=" + pathname.toLowerCase());
-  //     const { path } = window.Qs.parse(window.location.search, {
-  //       ignoreQueryPrefix: true,
-  //     });
-  //     setPathValue(path);
-  //     window.location = "#" + pathname.toLowerCase();
-  //     console.log(ref.current);
-  //     console.log(document.querySelector(".ul-nav  li"));
-  //   };
+  const handlePath = () => {
+    window.history.pushState("s", "", "?path=" + pathname.toLowerCase());
+    const { path } = window.Qs.parse(window.location.search, {
+      ignoreQueryPrefix: true,
+    });
+    // setPathValue(path);
+    // window.location = "#" + pathname.toLowerCase();
+    // console.log(ref.current);
+    // console.log(document.querySelector(".ul-nav  li"));
+    setPathValue(path);
+  };
   return (
-    <span ref={ref} className="li-nav">
+    <a
+      href={"#" + pathname.toLowerCase()}
+      className={
+        pathname.toLowerCase() === pathValue ? "li-navactive  li-nav" : "li-nav"
+      }
+      onClick={handlePath}>
       <li>{pathname}</li>
-    </span>
+    </a>
   );
 };
 
